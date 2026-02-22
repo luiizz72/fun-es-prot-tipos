5dbaf9428a627848d385683f255840f500c9a6d1
luiizz72 <169167567+luiizz72@users.noreply.github.com>
2026-02-21 22:07:31 -0300

    aula 4

---
+++ Admin.js
@@ -0,0 +1,13 @@
+import User from "./User.js";
+
+export default class Admin extends User {
+  constructor(nome, email, nascimento, role = "admin", ativo = "true") {
+    super(nome, email, nascimento, role, ativo)
+  }
+
+  criarCurso(nomeCurso, qtdVagas) {
+    return `curso ${nomeCurso} criado com ${qtdVagas} vagas.`
+  }
+}
+
+// console.log(novoAdmin.criarCurso('JavaScript', 20));
